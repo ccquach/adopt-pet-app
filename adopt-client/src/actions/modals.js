252c756e0d3988ce19e66c2bb4d108e328cb6e7b
cynthia.quach <ccquach93@gmail.com>
2018-05-03 10:43:32 -0700

    remove debuggers

--- adopt-client/src/actions/modals.js
+++ adopt-client/src/actions/modals.js
@@ -1,10 +1,7 @@
 export const SHOW_MODAL = "SHOW_MODAL";
 export const HIDE_MODAL = "HIDE_MODAL";
 
-// const API_URL = 'http://localhost:3001/api/pets/';
-
 export function showModal(pet) {
-  // debugger;
   return {
     type: SHOW_MODAL,
     pet
@@ -12,7 +9,6 @@ export function showModal(pet) {
 }
 
 export function hideModal() {
-  // debugger;
   return {
     type: HIDE_MODAL
   };
--- adopt-client/src/reducers/modals.js
+++ adopt-client/src/reducers/modals.js
@@ -3,10 +3,8 @@ import { SHOW_MODAL, HIDE_MODAL } from '../actions/modals';
 const modals = (state = false, action) => {
   switch (action.type) {
     case SHOW_MODAL:
-      // debugger;
       return true;
     case HIDE_MODAL:
-      // debugger;
       return false;
     default:
       return false;
