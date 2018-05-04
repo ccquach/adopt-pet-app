f425b5202c990cd4511bf395105db0f8660266aa
cynthia.quach <ccquach93@gmail.com>
2018-05-04 15:05:06 -0700

    load default img in pet item if img url not provided or url does not exist

--- adopt-client/src/components/Pet.js
+++ adopt-client/src/components/Pet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Pet.css';
 import PetContent from './PetContent';
 import IconButton from './IconButton';
+import notFoundImg from '../images/img_not_found.jpeg';
 
 const Pet = ({ pet, onShow, onEdit, onDelete }) => (
   <div className="pet-item-container">
@@ -15,7 +16,15 @@ const Pet = ({ pet, onShow, onEdit, onDelete }) => (
     />
     <div className="pet-item">
       <div className="pet-item-image">
-        <img src={pet.img} alt={pet.name} onClick={onShow} />
+        <img 
+          src={pet.img ? pet.img : notFoundImg} 
+          alt={pet.name} 
+          onError={(e) => {
+            e.target.onError = null;
+            e.target.src = notFoundImg
+          }}
+          onClick={onShow} 
+        />
       </div>
       <PetContent pet={pet} />
     </div>
