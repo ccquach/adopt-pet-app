11f6e45f50945a72820f6a364287c247f79c1aa0
cynthia.quach <ccquach93@gmail.com>
2018-05-11 18:20:19 -0700

    add rel attr to footer link for security measure

--- adopt-client/src/components/Footer.js
+++ adopt-client/src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = props => (
   <footer>
-    <span>Photos courtesy of <a href="https://www.unsplash.com" target="_blank">Unsplash</a></span>
+    <span>Photos courtesy of <a href="https://www.unsplash.com" target="_blank" rel="noopener noreferrer">Unsplash</a></span>
     <span>&copy;2018 Adopt a Pet App</span>
   </footer>
 );
